54d571f73bb4a6e65c2b3aa462babba7b9c1fc80
Flatiron School <flatironschool@Flatirons-MacBook-Air-4.local>
2018-07-19 15:00:35 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,58 @@
+require 'pry'
+require_relative "transfer"
+
 class BankAccount
+
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  @@all_accounts = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @valid = true
+    @@all_accounts << self
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    p  "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if  @balance <= 0 || @status == "closed"
+      @valid = false
+    else
+      @valid = true
+    end
+  end
+
+  def close_account
+      self.status = "closed"
+  end
+
+  # def self.valid?(sender, receiver)
+  #   if sender.valid? == true && receiver.valid? == true
+  #     Transfer.valid = true
+  #   end
+  # end
+
+  # def self.execute_transaction(sender, receiver, amount)
+  #   if Transfer.valid == true && Transfer.status != "complete"
+  #     sender.balance -= amount
+  #     receiver.balance += amount
+  #     Transfer.status = "complete"
+  #   end
+  #
+  #   if sender.valid? == false
+  #     puts "Transaction rejected. Please check your account balance."
+  #     Transfer.status = "rejected"
+  #   end
+  # end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,46 @@
+require_relative "bank_account"
+
 class Transfer
   # your code here
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :valid, :status
+
+  @@all_transfers = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    @valid = false
+    @@all_transfers << self
+  end
+
+  def valid?
+    if @sender.valid? == true && @receiver.valid? == true
+      self.valid = true
+    end
+  end
+
+  def execute_transaction
+    if self.valid? == true && self.status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      self.status = "complete"
+    end
+
+    if sender.valid? == false
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
+
 end
